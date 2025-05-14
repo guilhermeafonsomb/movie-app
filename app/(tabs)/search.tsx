28528655b27556e6140e5ae03e49160f47409c12
Guilherme <guilherme.bail@gauge.haus>
2025-05-14 16:54:20 -0300

    wip: making search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,61 @@
-import React from "react";
-import { Text, View } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import useFetch from "@/hooks/useFeatch";
+import { fetchMovies } from "@/services/api";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, View } from "react-native";
 
 const Search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+        source={images.bg}
+      />
+
+      <FlatList
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image className="w-12 h-10" source={icons.logo} />
+            </View>
+
+            <View className="my-5">
+              <SearchBar placeholder="Search movies ..." />
+            </View>
+
+            {moviesLoading && (
+              <ActivityIndicator
+                className="my-3"
+                size="large"
+                color="#0000FF"
+              />
+            )}
+          </>
+        }
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <MovieCard {...item} />}
+      />
     </View>
   );
 };
